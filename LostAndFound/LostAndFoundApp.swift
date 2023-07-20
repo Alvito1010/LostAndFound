8b8bef79921392ce2b13b4e987846eaa4b76b82d
dekapd99 <dekaprimatio@gmail.com>
2023-07-20 12:30:30 +0700

    Initial Commit

---
+++ LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Deka Primatio on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
