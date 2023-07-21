174c5be1eda80815f468b15c3a451a05257c77de
Alvito1010 <alvito1010@gmail.com>
2023-07-21 11:14:23 +0700

    writeViewModel selesai

--- LostAndfound/ViewModel/WriteViewModel.swift
+++ LostAndfound/ViewModel/WriteViewModel.swift
@@ -20,6 +20,20 @@ class WriteViewModel: ObservableObject{
     func createPath(nama: String, hp: String, jenis: String, rutePerjalanan: String, deskripsi: String, detailLokasi: String, waktu: String) {
         var i = 0
         
+        //time function
+        
+        func getCurrentTime() -> String {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "HH:mm"
+                return dateFormatter.string(from: Date())
+            }
+        
+        func getCurrentDate() -> String {
+                let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "MMMM d"
+                    return dateFormatter.string(from: Date())
+            }
+        
         // Define a function to recursively check for the next available node
         func checkNextNode() {
             ref.child("\(i)").observeSingleEvent(of: .value) { snapshot in
@@ -29,7 +43,7 @@ class WriteViewModel: ObservableObject{
                     checkNextNode() // Recursive call to check the next number
                 } else {
                     // Node with the current number does not exist, create the node
-                    let newNodeRef = self.ref.child("\(i)")
+                    let newNodeRef = self.ref.child("\(i)/laporan")
                     newNodeRef.child("nama").setValue(nama)
                     newNodeRef.child("hp").setValue(hp)
                     newNodeRef.child("jenis").setValue(jenis)
@@ -37,6 +51,41 @@ class WriteViewModel: ObservableObject{
                     newNodeRef.child("deskripsi").setValue(deskripsi)
                     newNodeRef.child("detailLokasi").setValue(detailLokasi)
                     newNodeRef.child("waktu").setValue(waktu)
+                    
+                    let status1NodeRef = self.ref.child("\(i)/laporanDibuat")
+                    status1NodeRef.child("waktu").setValue("\(getCurrentTime())")
+                    status1NodeRef.child("tanggal").setValue("\(getCurrentDate())")
+                    status1NodeRef.child("bool").setValue(true)
+                    
+                    let status2NodeRef = self.ref.child("\(i)/laporanDiterima")
+                    status2NodeRef.child("waktu").setValue("")
+                    status2NodeRef.child("tanggal").setValue("")
+                    status2NodeRef.child("bool").setValue(false)
+                    
+                    let status3NodeRef = self.ref.child("\(i)/prosesPencarian")
+                    status3NodeRef.child("waktu").setValue("")
+                    status3NodeRef.child("tanggal").setValue("")
+                    status3NodeRef.child("bool").setValue(false)
+                    
+                    let status4NodeRef = self.ref.child("\(i)/pencarianTahapDua")
+                    status4NodeRef.child("waktu").setValue("")
+                    status4NodeRef.child("tanggal").setValue("")
+                    status4NodeRef.child("bool").setValue(false)
+                    
+                    let status5NodeRef = self.ref.child("\(i)/barangTelahDitemukan")
+                    status5NodeRef.child("waktu").setValue("")
+                    status5NodeRef.child("tanggal").setValue("")
+                    status5NodeRef.child("bool").setValue(false)
+                    
+                    let status6NodeRef = self.ref.child("\(i)/barangTelahDiambil")
+                    status6NodeRef.child("waktu").setValue("")
+                    status6NodeRef.child("tanggal").setValue("")
+                    status6NodeRef.child("bool").setValue(false)
+                    
+                    let status7NodeRef = self.ref.child("\(i)/barangTidakDitemukan")
+                    status7NodeRef.child("waktu").setValue("")
+                    status7NodeRef.child("tanggal").setValue("")
+                    status7NodeRef.child("bool").setValue(false)
                 }
             }
         }
